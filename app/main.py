8f71de0203a37d512e4a54c0c0d9f7a58a4a91fc
thassung <105700459+thassung@users.noreply.github.com>
2024-03-28 01:35:49 +0700

    first commit

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, jsonify
+from script import *
+
+app = Flask(__name__, template_folder='templates')
+
+@ app.route('/', methods=['POST', 'GET'])
+def main():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+@ app.route('/generate', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        inputData = request.json
+        instruction = inputData['instruction'] 
+        max_length = inputData['max_length'] 
+        temperature = inputData['temperature'] 
+
+        generated_text = generate_text(instruction=instruction,
+                                       max_length=max_length,
+                                       temperature=temperature)
+
+        return jsonify({'generated_text': generated_text})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
---
+++ app/script.py
@@ -0,0 +1,43 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+file_path = '.'
+model_path = f'{file_path}/model/checkpoint-625'
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_path, device_map = 'auto')
+
+tokenizer = AutoTokenizer.from_pretrained(
+    model_path)
+
+text_generator = pipeline(
+    task='text-generation',
+    model=model,
+    tokenizer=tokenizer,
+    device_map = 'auto',
+    # Setting `pad_token_id` to `eos_token_id`:50256 for open-end generation.
+    pad_token_id = tokenizer.eos_token_id
+)
+
+def format_prompt(instruction):
+    input = ''
+    return f"""
+Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{instruction}
+
+### Input:
+{input}
+
+### Output:
+""".strip()
+
+	
+
+def generate_text(instruction, max_length, temperature):
+    generated_text = text_generator(format_prompt(instruction),
+                                    max_length = max_length,
+                                    temperature = temperature)
+    generated_text = generated_text[0]['generated_text']
+    generated_output = generated_text.split('### Output:\n')[-1]
+    return generated_output
